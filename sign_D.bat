f61102efcada8ab4b22ec82085f193549de08491
jbreslin33 <jbreslin33@localhost>
2011-07-18 13:27:56 -0400

    nice. sign works. i guess now it's just hopefully copying files, uploading and then test. paths may need modifying.

--- sign_D.bat
+++ sign_D.bat
@@ -1,1 +1,5 @@
-keytool -genkey -alias alias_breslin -keystore keystore_breslin < keystore.conf
+jarsigner -keystore keystorebreslin -storepass mibesfat -keypass mibesfat lib/breslin.jar breslin
+jarsigner -keystore keystorebreslin -storepass mibesfat -keypass mibesfat lib/jME3-lwjgl-natives.jar breslin
+jarsigner -keystore keystorebreslin -storepass mibesfat -keypass mibesfat lib/jMonkeyEngine3.jar breslin
+jarsigner -keystore keystorebreslin -storepass mibesfat -keypass mibesfat lib/lwjgl.jar breslin
+jarsigner -keystore keystorebreslin -storepass mibesfat -keypass mibesfat lib/windows-native.jar breslin
