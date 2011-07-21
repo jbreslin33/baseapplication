5c430f0092bf6e1aadfbb04651cad957b49b56f0
jbreslin33 <jbreslin33@localhost>
2011-07-21 16:59:42 -0400

    dfdf
    dfdf

--- src/breslin/game/Game.java
+++ src/breslin/game/Game.java
@@ -20,13 +20,18 @@ import java.io.*;
 /** Sample 5 - how to map keys and mousebuttons to actions */
 public class Game extends SimpleApplication {
 
+//join game
+boolean sentJoinGame     = false;
+boolean receivedJoinGame = false;
 
-	String mServerIP;
+//network
+String mServerIP;
 
-	Game(String serverIP) {
-
-		mServerIP = serverIP;
-	}
+//constructor
+Game(String serverIP)
+{
+	mServerIP = serverIP;
+}
 
   public static void main(String[] args) {
 
@@ -34,8 +39,6 @@ public class Game extends SimpleApplication {
    		System.out.println("You need to pass in serverIP as parameter");
 	    return;
    }
-
-
 
     Game app = new Game(args[0]);
     app.start();
@@ -55,7 +58,12 @@ public class Game extends SimpleApplication {
     rootNode.attachChild(player);
     initKeys(); // load my custom keybinding
 
+	//joinGame();
 
+	}
+
+void joinGame()
+{
 	int messageCount = 0;
     //networking
     try {
@@ -67,7 +75,7 @@ public class Game extends SimpleApplication {
     byte[] buf = new byte[1400];
 
 	//write to message
-    buf[0] = 'W';
+    buf[0] = 'j';
 	messageCount++;
 
 	//now make new buffer so that it's only as big as needed.
@@ -94,19 +102,22 @@ public class Game extends SimpleApplication {
 		e.printStackTrace();
     }
 
-  }
+}
 
   /** Custom Keybinding: Map named actions to inputs. */
   private void initKeys() {
     // You can map one or several inputs to one named action
     inputManager.addMapping("Pause",  new KeyTrigger(KeyInput.KEY_P));
-    inputManager.addMapping("Left",   new KeyTrigger(KeyInput.KEY_J));
-    inputManager.addMapping("Right",  new KeyTrigger(KeyInput.KEY_K));
+    inputManager.addMapping("Left",   new KeyTrigger(KeyInput.KEY_K));
+    inputManager.addMapping("Right",  new KeyTrigger(KeyInput.KEY_L));
     inputManager.addMapping("Rotate", new KeyTrigger(KeyInput.KEY_SPACE),
                                       new MouseButtonTrigger(MouseInput.BUTTON_LEFT));
+    inputManager.addMapping("Join",   new KeyTrigger(KeyInput.KEY_J));
+
     // Add the names to the action listener.
     inputManager.addListener(actionListener, new String[]{"Pause"});
-    inputManager.addListener(analogListener, new String[]{"Left", "Right", "Rotate"});
+    inputManager.addListener(analogListener, new String[]{"Join", "Left", "Right", "Rotate"});
+
 
   }
 
@@ -121,6 +132,11 @@ public class Game extends SimpleApplication {
   private AnalogListener analogListener = new AnalogListener() {
     public void onAnalog(String name, float value, float tpf) {
       if (isRunning) {
+        if (name.equals("Join"))
+        {
+        	joinGame();
+        }
+
         if (name.equals("Rotate")) {
           player.rotate(0, value*speed, 0);
         }
