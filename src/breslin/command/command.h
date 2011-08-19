1985b168dee1f25a7379c7b8f577b7acce0f94f0
jbreslin33 <jbreslin33@localhost>
2011-08-18 22:44:25 -0400

    dfdfd

--- src/breslin/command/command.h
+++ src/breslin/command/command.h
@@ -1,14 +1,27 @@
 #ifndef COMMAND_H
 #define COMMAND_H
 
+/******************************************************
+*				INCLUDES
+********************************************************/
 #include "../math/vector3D.h"
 
+/******************************************************
+*				FORWARD DECLARATIONS
+********************************************************/
+
+/******************************************************
+*				CLASS
+********************************************************/
 class Command
 {
 public:
 Command();
 ~Command();
-	
+
+/******************************************************
+*				VARIABLES
+********************************************************/
 Vector3D mOrigin;            //finish origin of frame/tick
 Vector3D mOriginOld;            //origin of last frame/tick
 Vector3D mVelocity;	         //velocity during frame/tick
@@ -22,5 +35,9 @@ int mMilliseconds;      //not used
 int mMillisecondsTotal;      //not used
 
 float mClientFrametime;
+
+/******************************************************
+*				METHODS
+********************************************************/
 };
 #endif
