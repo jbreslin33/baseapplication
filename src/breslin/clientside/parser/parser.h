10afa0e4b53c4759c5b67b45ac91e4508f177fea
jbreslin33 <jbreslin33@localhost>
2011-08-22 18:22:40 -0400

    added parser class for realss

---
+++ src/breslin/clientside/parser/parser.h
@@ -0,0 +1,45 @@
+#ifndef PARSER_H
+#define PARSER_H
+
+/***************************************
+*   		INCLUDES
+***************************************/
+//standard library
+#include <vector>
+
+//parents
+
+
+/***************************************
+*   		FORWARD DECLARATIONS
+***************************************/
+
+class Parser 
+{
+public:
+	
+	Parser();
+	~Parser();
+
+/***************************************
+*   		MEMBER VARIABLES
+***************************************/
+
+// Dispatches
+static const char mCommandKey          = 1;
+static const char mCommandMilliseconds = 2;
+
+static const char mMessageFrame = 1;
+static const char mMessageConnect     = -101;
+static const char mMessageDisconnect  = -102;
+static const char mMessageAddShape    = -103;
+static const char mMessageRemoveShape = -104;
+
+static const char mMessageNonDeltaFrame = 2;
+static const char mMessageServerExit = 3;
+static const char mMessageKeepAlive = 12;
+
+
+};
+
+#endif
