222ca5e7ad03041c7ea47994f3dfcc300b3c6978
jbreslin33 <jbreslin33@localhost>
2011-09-27 08:33:18 -0400

    delete

--- run_nifty-gui.bat
+++
@@ -1,3 +0,0 @@
-cd build
-java -cp "../lib/jME3-lwjgl-natives.jar;../lib/jMonkeyEngine3.jar;../lib/lwjgl.jar;../lib/nifty-1.3.jar;../lib/nifty-lwjgl-renderer-1.3-javadoc.jar;." breslin/clientside/game/TestNiftyGui
-cd ..
