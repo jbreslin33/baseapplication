b97ff4ac0658b861c098d2c5123a174571cbc3f0
jbreslin33 <jbreslin33@localhost>
2011-09-27 08:14:59 -0400

    change jar Main class to GameMonkey.

--- createBreslinJar_B.bat
+++ createBreslinJar_B.bat
@@ -1,3 +1,3 @@
 cd build
-jar -cfm ..\lib\breslin.jar ..\src\Manifest.txt breslin\clientside\game\Game.class
+jar -cfm ..\lib\breslin.jar ..\src\Manifest.txt breslin\clientside\game\GameMonkey.class
 cd ..
