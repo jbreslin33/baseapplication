bf7a0688a953c6ba7d575c33fbc7382d1024a828
jbreslin33 <jbreslin33@localhost>
2011-10-11 20:34:22 -0400

    u

--- deleteAll_1.sh
+++
@@ -1,3 +0,0 @@
-rm -rf build/
-rm -rf lib/
-rm -rf dist/
--- run_4.bat
+++
@@ -1,4 +0,0 @@
-cd build
-java -cp "../lib/jME3-lwjgl-natives.jar;../lib/jMonkeyEngine3.jar;../lib/lwjgl.jar;." breslin/clientside/game/GameMonkey 192.168.1.103
-cd ..
-
