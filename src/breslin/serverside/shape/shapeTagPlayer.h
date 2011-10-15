ee8c5719c55d54e7af6ef1ed9d9c924023f53ba4
jbreslin33 <jbreslin33@localhost>
2011-10-14 21:16:48 -0400

    u

---
+++ src/breslin/serverside/shape/shapeTagPlayer.h
@@ -0,0 +1,17 @@
+#ifndef SHAPETAG_H
+#define SHAPETAG_H
+
+#include "shape.h"
+
+class ShapeTag : public	Shape
+{
+
+public:
+ShapeTag(Vector3D* position, Vector3D* velocity, Vector3D* rotation, Ogre::Root* root,unsigned int index,
+	  bool animated, bool collidable, float collisionRadius, int meshCode, bool ai);
+~ShapeTag();
+
+
+};
+
+#endif
