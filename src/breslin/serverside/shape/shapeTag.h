929e61c76e7403406a5d2785a5f1febf161836ac
jbreslin33 <jbreslin33@localhost>
2011-10-14 21:07:52 -0400

    added shapeTag. this extends shape for any special methods or vars are needed for tag shapes.

---
+++ src/breslin/serverside/shape/shapeTag.h
@@ -0,0 +1,16 @@
+#ifndef SHAPETAG_H
+#define SHAPETAG_H
+
+#include "shape.h"
+
+class ShapeTag : public	Shape
+{
+
+public:
+ShapeTag(Vector3D* position, Vector3D* velocity, Vector3D* rotation, Ogre::Root* root,unsigned int index,
+	  bool animated, bool collidable, float collisionRadius, int meshCode, bool ai);
+~ShapeTag();
+
+};
+
+#endif
