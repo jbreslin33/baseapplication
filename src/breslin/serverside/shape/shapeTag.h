669428528fa7fc5cb0c1f4686d2b21ef8a56cf42
jbreslin33 <jbreslin33@localhost>
2011-10-25 17:14:37 -0400

    u

--- src/breslin/serverside/shape/shapeTag.h
+++ src/breslin/serverside/shape/shapeTag.h
@@ -7,7 +7,7 @@ class ShapeTag : public	Shape
 {
 
 public:
-ShapeTag(Game* game, Client* client, Vector3D* position, Vector3D* velocity, Vector3D* rotation, Ogre::Root* root,unsigned int index,
+ShapeTag(Game* game, Client* client, Vector3D* position, Vector3D* velocity, Vector3D* rotation, Ogre::Root* root,
 	  bool animated, bool collidable, float collisionRadius, int meshCode, bool ai);
 ~ShapeTag();
 
