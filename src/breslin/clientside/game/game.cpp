bff9ee597adbc1816f397857d17e2b3f01d5baee
jbreslin33 <jbreslin33@localhost>
2011-11-12 10:27:58 -0500

    added Game.

---
+++ src/breslin/clientside/game/game.cpp
@@ -0,0 +1,15 @@
+//header
+#include "game.h"
+
+/***************************************
+*			          CONSTRUCTORS
+***************************************/
+Game::Game(const char* serverIP, int serverPort)
+{
+
+}
+
+Game::~Game()
+{
+
+}
