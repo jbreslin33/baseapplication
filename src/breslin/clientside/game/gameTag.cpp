98dc941941e83ffe2c739bb199aa81835981acfe
jbreslin33 <jbreslin33@localhost>
2011-11-19 10:22:59 -0500

    u

---
+++ src/breslin/clientside/game/gameTag.cpp
@@ -0,0 +1,99 @@
+//header
+#include "game.h"
+
+//application
+#include "../game/application.h"
+
+//shape
+#include "../shape/shape.h"
+
+//bytebuffer
+#include "../bytebuffer/byteBuffer.h"
+
+//ability
+#include "../ability/rotation/abilityRotation.h"
+#include "../ability/move/abilityMove.h"
+
+/***************************************
+*			          CONSTRUCTORS
+***************************************/
+Game::Game(Application* application,const char* serverIP, int serverPort)
+{
+	mApplication = application;
+}
+
+Game::~Game()
+{
+
+}
+/*********************************
+		TICK
+**********************************/
+void Game::run()
+{
+	for (unsigned int i = 0; i < mShapeVector.size(); i++)
+	{
+		mShapeVector.at(i)->interpolateTick(mApplication->getRenderTime());
+	}
+}
+
+void Game::removeShape(ByteBuffer* byteBuffer)
+{
+	int index = byteBuffer->ReadByte();
+	
+	Shape* shape = getShape(index);
+
+	for (unsigned int i = 0; i < mShapeVector.size(); i++)
+	{
+		if (mShapeVector.at(i) == shape)
+		{
+			delete mShapeVector.at(i);
+			mShapeVector.erase (mShapeVector.begin()+i);
+		}
+	}
+}
+
+Shape* Game::getShape(int id)
+{
+	Shape* shape = NULL;
+
+	for (unsigned int i = 0; i < mShapeVector.size(); i++)
+	{
+		Shape* curShape = mShapeVector.at(i);
+		if (curShape->mIndex == id)
+		{
+			shape = curShape;
+		}
+	}
+
+	if(!shape)
+	{
+		return NULL;
+	}
+	else
+	{
+		return shape;
+	}
+}
+
+/*************************************************
+*
+*   OGRE_SPECIFIC
+*
+**************************************************
+
+/*********************************
+*		SHAPE
+**********************************/
+void Game::addShape(bool b, ByteBuffer* byteBuffer)
+{
+	Shape* shape = new Shape(mApplication,byteBuffer,false);  //you should just need to call this...
+	kjkj need to make GameTag so you can overide addShape and create a shapeTag
+	//ability
+	shape->addAbility(new AbilityRotation(shape));
+	shape->addAbility(new AbilityMove(shape));
+
+	//put shape and ghost in game vectors so they can be looped and game now knows of them.
+	mShapeVector.push_back(shape);
+	mShapeGhostVector.push_back(shape->mGhost);	
+}
---
+++ src/breslin/clientside/game/gameTag.h
@@ -0,0 +1,61 @@
+#ifndef GAME_H
+#define GAME_H
+/**************************************
+@Game: This class should handle game specific duties. It should be able to be destroyed without
+the application crashing. You should be able to switch games while the application is running.
+
+Since this is on the client side it does not need any game logic.
+
+It's main duties will be to track objects to be drawn(Shapes). When it is destroyed or
+reset these objects need to be destroyed.
+
+*************************************/
+
+
+/***************************************
+*   		INCLUDES
+***************************************/
+//standard library
+#include <vector>
+
+/***************************************
+*   		FORWARD DECLARATIONS
+***************************************/
+class Application;
+class ByteBuffer;
+class Shape;
+
+class Game 
+{
+public:
+	
+	Game(Application* application, const char* serverIP, int serverPort);
+	~Game();
+
+/***************************************
+*   		MEMBER VARIABLES
+***************************************/
+private:
+//application
+Application* mApplication;
+
+//Shapes
+std::vector<Shape*> mShapeVector;	 //all shapes in the client world
+std::vector<Shape*> mShapeGhostVector;	 //all shapes in the client world's ghost 
+
+	
+/***************************************
+*			          METHODS
+***************************************/
+public:
+
+//shape
+void   addShape       (bool b, ByteBuffer* byteBuffer);
+void   removeShape    (ByteBuffer* byteBuffer);
+Shape* getShape(int id);
+
+void run();
+
+};
+
+#endif
