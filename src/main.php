ae6e87cb913f311ee06d2c54112cb8615e196dbb
jbreslin33 <jbreslin33@localhost>
2012-12-26 21:34:00 -0500

    adding mootols and first steps

--- src/main.php
+++ src/main.php
@@ -1,3 +1,12 @@
+<?php
+<!-- mootools -->
+<script type="text/javascript" src="/src/mootools/mootools-core-1.4.5-full-compat.js"></script>
+
+
+
+?>
+
+
 <!DOCTYPE html>
 <meta http-equiv="Content-type" content="text/html;charset=UTF-8">
 <html>
