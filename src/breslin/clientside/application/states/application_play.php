8f7b4f48ba3a6957ead968784810e7113954860b
jbreslin33 <jbreslin33@localhost>
2012-12-27 10:59:53 -0500

    filled out play state but commented out cause it's still in c++

--- src/breslin/clientside/application/states/application_play.php
+++ src/breslin/clientside/application/states/application_play.php
@@ -13,12 +13,31 @@ enter: function()
 
 execute: function()
 {
-
+/*
+       if (mApplicationBreslin->getKeyboard()->isKeyDown(OIS::KC_ESCAPE))
+        {
+                mApplicationBreslin->mPlayingGame = false;
+                mApplicationBreslin->mStateMachine->changeState(mApplicationBreslin->mApplicationMain);
+        }
+        else
+        {
+                //game
+                mApplicationBreslin->mGame->processUpdate();
+        }
+*/
 },
 
 exit: function()
 {
+/*
+     mApplicationBreslin->shutdown();
 
+        mApplicationBreslin->getSceneManager()->destroyAllEntities();
+        delete mApplicationBreslin->mGame;
+
+        mApplicationBreslin->showMainScreen();
+
+*/
 }
 
 });
