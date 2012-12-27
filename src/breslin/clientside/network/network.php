72f1b0a9a9839e795aec96d15ed5e61764fbba9c
jbreslin33 <jbreslin33@localhost>
2012-12-26 22:54:33 -0500

    network

---
+++ src/breslin/clientside/network/network.php
@@ -0,0 +1,45 @@
+var Network = new Class(
+{
+
+initialize: function(application,serverIP, serverPort)
+{
+        //application
+        this.mApplicationBreslin = application;
+
+        //server address
+        this.mServerIP = serverIP;
+        this.mServerPort = serverPort;
+
+        //sequences
+        this.mIncomingSequence               = 0;
+
+	this.mSocket = this.open();
+
+	//parse
+        this.mIncomingSequence = 0;
+        this.mDroppedPackets = 0;
+			
+},
+
+log: function(msg)
+{
+        setTimeout(function()
+	{
+        	throw new Error(msg);
+        }, 0);
+},
+
+// this should call ajax function on server to create a socket
+open: function()
+{
+	
+},
+
+//this should call ajax function on server to send
+send: function(byteBuffer)
+{
+
+}
+
+});
+
