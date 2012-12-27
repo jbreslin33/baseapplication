3e07e9141c63f450adae7ba805a09e7d27bfe1f1
jbreslin33 <jbreslin33@localhost>
2012-12-26 23:23:26 -0500

    adding application states

---
+++ src/breslin/clientside/application/states/application_initialize.php
@@ -0,0 +1,46 @@
+//header
+#include "applicationInitialize.h"
+
+//log
+#include "../../tdreamsock/dreamSockLog.h"
+
+//state machine
+#include "../../../statemachine/stateMachine.h"
+
+//application
+#include "../applicationBreslin.h"
+
+/***************************************
+*	CONSTRUCTORS		          
+***************************************/
+ApplicationInitialize::ApplicationInitialize(ApplicationBreslin* applicationBreslin)
+{
+	mApplicationBreslin = applicationBreslin;
+}
+
+ApplicationInitialize::~ApplicationInitialize()
+{
+
+}
+
+void ApplicationInitialize::enter()
+{
+	LogString("STATE: ApplicationInitialize");
+}
+
+void ApplicationInitialize::execute()
+{
+	//setup calls ogre specific graphics setup,
+	//when it returns true we can begin our graphics stuff
+	if (mApplicationBreslin->setup())
+	{
+		mApplicationBreslin->mSetup = true;
+		mApplicationBreslin->createMainScreen();
+        mApplicationBreslin->mStateMachine->changeState(mApplicationBreslin->mApplicationMain);
+	}	
+}
+
+void ApplicationInitialize::exit()
+{
+
+}
---
+++ src/breslin/clientside/application/states/application_main.php
@@ -0,0 +1,91 @@
+//header
+#include "applicationMain.h"
+
+
+//log
+#include "../../tdreamsock/dreamSockLog.h"
+
+//application
+#include "../applicationBreslin.h"
+
+//game
+#include "../../game/game.h"
+#include "../../game/gameTag.h"
+
+//state machine
+#include "../../../statemachine/stateMachine.h"
+
+/***************************************
+*	CONSTRUCTORS
+***************************************/
+ApplicationMain::ApplicationMain(ApplicationBreslin* applicationBreslin)
+{
+	mApplicationBreslin = applicationBreslin;
+}
+
+ApplicationMain::~ApplicationMain()
+{
+
+}
+
+void ApplicationMain::enter()
+{
+	LogString("STATE: ApplicationGlobal");
+	mApplicationBreslin->showMainScreen();
+}
+
+void ApplicationMain::execute()
+{
+	if (mApplicationBreslin->mButtonHit == mApplicationBreslin->mButtonGame)
+	{
+		mApplicationBreslin->mButtonHit = NULL;
+		mApplicationBreslin->sendConnect();
+		mApplicationBreslin->mGame = new Game(mApplicationBreslin);
+
+		mApplicationBreslin->hideMainScreen();
+
+		mApplicationBreslin->mStateMachine->changeState(mApplicationBreslin->mApplicationPlay);
+	}
+
+	if (mApplicationBreslin->mButtonHit == mApplicationBreslin->mButtonTag)
+	{
+		mApplicationBreslin->mButtonHit = NULL;
+		mApplicationBreslin->sendConnect();
+		mApplicationBreslin->mGame = new GameTag(mApplicationBreslin);
+
+		mApplicationBreslin->hideMainScreen();
+
+		mApplicationBreslin->mStateMachine->changeState(mApplicationBreslin->mApplicationPlay);
+	}
+
+	if (mApplicationBreslin->mButtonHit == mApplicationBreslin->mButtonTagAll)
+	{
+		mApplicationBreslin->mButtonHit = NULL;
+		mApplicationBreslin->sendConnect();
+		mApplicationBreslin->mGame = new GameTag(mApplicationBreslin);
+
+		mApplicationBreslin->hideMainScreen();
+
+		mApplicationBreslin->mStateMachine->changeState(mApplicationBreslin->mApplicationPlay);
+	}
+	
+	if (mApplicationBreslin->mButtonHit == mApplicationBreslin->mButtonExit)
+	{
+
+		mApplicationBreslin->mStateMachine->changeState(NULL);
+		mApplicationBreslin->mStateMachine->setGlobalState(NULL);
+
+		mApplicationBreslin->mButtonHit = NULL;
+
+		mApplicationBreslin->shutdown();
+
+		mApplicationBreslin->mShutDown = true;
+
+		delete mApplicationBreslin;
+	}
+}
+
+void ApplicationMain::exit()
+{
+
+}
