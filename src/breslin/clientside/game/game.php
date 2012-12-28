1b00c75d1a935fdc12b7f0e98212fef40fee0d5a
jbreslin33 <jbreslin33@localhost>
2012-12-27 22:22:42 -0500

    looks like game works so far

--- src/breslin/clientside/game/game.php
+++ src/breslin/clientside/game/game.php
@@ -1,8 +1,52 @@
 var Game = new Class(
 {
 
-initialize: function()
+initialize: function(applicationBreslin)
 {
+/*
+        mStateMachine = new StateMachine();
+        mGameGlobal = new GameGlobal(this);
+        mGameInitialize = new GameInitialize(this);
+        mGamePlay = new GamePlay(this);
+        mGamePause = new GamePause(this);
+
+        mStateMachine->setGlobalState(mGameGlobal);
+        mStateMachine->changeState(mGamePlay);
+
+        //set Camera
+        // Position it at 500 in Z direction
+        mApplicationBreslin->getCamera()->setPosition(Ogre::Vector3(0,20,20));
+        // Look back along -Z
+        mApplicationBreslin->getCamera()->lookAt(Ogre::Vector3(0,0,0));
+        mApplicationBreslin->getCamera()->setNearClipDistance(5);
+*/
+	
+	this.mApplicationBreslin = applicationBreslin;	
+
+	this.mShapeVector      = new Array();
+	this.mShapeGhostVector = new Array();
+
+        //keys
+        this.mKeyUp = 1;
+        this.mKeyDown = 2;
+        this.mKeyLeft = 4;
+        this.mKeyRight = 8;
+        this.mKeyCounterClockwise = 16;
+        this.mKeyClockwise = 32;
+
+        //input
+        this.mKeyCurrent = 0;
+        this.mKeyLast = 0;
+
+        //sequence
+        this.mOutgoingSequence               = 1;
+
+        //time
+        this.mRunNetworkTime = 0.0;
+
+       	//states 
+	this.mStateMachine = new StateMachine();
+
 
 },
 
