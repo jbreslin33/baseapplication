b40a07044e73acae700fba5949d32e9a55804dd4
jbreslin33 <jbreslin33@localhost>
2012-12-27 20:55:14 -0500

    checking if mmessageConnect

--- src/breslin/clientside/network/send.php
+++ src/breslin/clientside/network/send.php
@@ -1,6 +1,24 @@
 <?php
 $bytebuffer = $_POST["bytebuffer"];
 
+//is this where you want to send back server data from game? or should we use seperate ajax calls? 
+//scratch that for a minute i think you need to interject here and..
+//ask if this is a mMessageConnect and if so
+//then ask the db for a client_id
+//then send back the client_id...
+//and tell db that you got it.
+
+if ($bytebuffer == -101)
+{
+	echo 'mMessageConnect';
+}
+else
+{
+	echo 'somethingElse';
+}
+
+
+
 //have to create this every time because it cannot be save in a session variable as it's a resource
 $sock = socket_create(AF_INET, SOCK_DGRAM,0);
 
@@ -10,7 +28,6 @@ $len = strlen($packed);
 socket_sendto($sock, $packed, $len, 0, '192.168.1.100', 30004);
 socket_close($sock);
 
-//is this where you want to send back server data from game? or should we use seperate ajax calls? 
 
 ?>
 
