f7cd6c7f98ea6625d89a57c0ed7eeafb43285bd9
jbreslin33 <jbreslin33@localhost>
2012-12-27 21:22:49 -0500

    added client table to db

--- src/breslin/clientside/network/send.php
+++ src/breslin/clientside/network/send.php
@@ -11,6 +11,14 @@ $bytebuffer = $_POST["bytebuffer"];
 if ($bytebuffer == -101)
 {
 	echo 'mMessageConnect';
+	
+	//get current client id then update record 
+	$conn = pg_connect("host=localhost dbname=abcandyou user=postgres password=mibesfat")
+        or die('Could not connect: ' . pg_last_error());
+
+
+	
+
 }
 else
 {
