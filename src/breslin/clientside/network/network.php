ed6a8c7dae5414ee3d3f50fa513941a51aa9fd27
jbreslin33 <jbreslin33@localhost>
2012-12-27 21:43:40 -0500

    cleanup

--- src/breslin/clientside/network/network.php
+++ src/breslin/clientside/network/network.php
@@ -40,24 +40,26 @@ send: function(b)
 {
 	this.log('send before xmlHttprequest');
 	var xmlhttp;
-if (window.XMLHttpRequest)
-  {// code for IE7+, Firefox, Chrome, Opera, Safari
-  xmlhttp=new XMLHttpRequest();
-  }
-else
-  {// code for IE6, IE5
-  xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
-  }
-xmlhttp.onreadystatechange=function()
-  {
-  if (xmlhttp.readyState==4 && xmlhttp.status==200)
-    {
-    document.getElementById("demo").innerHTML=xmlhttp.responseText;
-    }
-  }
-xmlhttp.open("POST","/breslin/clientside/network/send.php",true);
-xmlhttp.setRequestHeader("Content-type","application/x-www-form-urlencoded");
-xmlhttp.send("bytebuffer=" + b.mBuffer);
+	if (window.XMLHttpRequest)
+  	{
+		// code for IE7+, Firefox, Chrome, Opera, Safari
+  		xmlhttp=new XMLHttpRequest();
+  	}
+	else
+  	{
+		// code for IE6, IE5
+  		xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
+  	}
+	xmlhttp.onreadystatechange=function()
+  	{
+  		if (xmlhttp.readyState==4 && xmlhttp.status==200)
+    		{
+    			document.getElementById("demo").innerHTML=xmlhttp.responseText;
+    		}
+  	}
+	xmlhttp.open("POST","/breslin/clientside/network/send.php",true);
+	xmlhttp.setRequestHeader("Content-type","application/x-www-form-urlencoded");
+	xmlhttp.send("bytebuffer=" + b.mBuffer);
 }
 
 });
