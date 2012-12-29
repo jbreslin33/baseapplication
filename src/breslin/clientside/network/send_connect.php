3cddaa91815c8dea84ffbaad3c9da7d20586bfed
jbreslin33 <jbreslin33@localhost>
2012-12-28 22:15:06 -0500

    cleanup and changed php connect to char

--- src/breslin/clientside/network/send_connect.php
+++ src/breslin/clientside/network/send_connect.php
@@ -12,6 +12,8 @@ $client_id = 0;
 
 $messageConnect = -111; 
 
+
+//*****************DATABASE
 //get current client id then update record 
 $conn = pg_connect("host=localhost dbname=abcandyou user=postgres password=mibesfat")
        or die('Could not connect: ' . pg_last_error());
@@ -31,15 +33,19 @@ $query .= $new_client_id;
 $query .= " where id = 1";	
 
 $result = pg_query($conn,$query);
+
+//send client id back to client browser
 echo $client_id;
 
 
 
 
+//********************send to c++ server
+
 //have to create this every time because it cannot be save in a session variable as it's a resource
 $sock = socket_create(AF_INET, SOCK_DGRAM,0);
 
-$packed = pack("i",$messageConnect);
+$packed = pack("c",$messageConnect);
 $len = strlen($packed);
 
 socket_sendto($sock, $packed, $len, 0, '192.168.1.100', 30004);
