40f38b2aa9b875c8ecc743934cc1b036018aa9ae
jbreslin33 <jbreslin33@localhost>
2012-12-29 22:18:29 -0500

    think i found bug it's with the int to str conversion

--- src/breslin/serverside/shape/shape.cpp
+++ src/breslin/serverside/shape/shape.cpp
@@ -86,10 +86,16 @@ Shape::Shape(unsigned int index, Game* game, Client* client, Vector3D* position,
 	sendShapeToClients();
 
 	const char* one = "insert into shapes values (";
-	const char* two = (char*) mIndex;
+	//const char* two = (char*) mIndex;
+	const char* two = "1";
 	const char* three = ",0.0,0.0,0.0,0.0,0.0)";
- 	
-	std::string str="insert into shapes values (1,0.0,0.0,0.0,0.0,0.0)";
+ 
+	std::string str;
+	str.append(one);
+	str.append(two);
+	str.append(three);
+	
+	//std::string str="insert into shapes values (1,0.0,0.0,0.0,0.0,0.0)";
 	const char * c = str.c_str();
 	mGame->sqlQuery(c);
 
