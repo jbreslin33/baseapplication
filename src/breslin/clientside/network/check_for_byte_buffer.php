468250724a22957bf613e23d4c7fa20bd3c47b16
jbreslin3 <jbreslin3@localhost>
2012-12-30 13:31:46 -0500

    a little lag but we have the numbers on the client side. how to get frame time?

--- src/breslin/clientside/network/check_for_byte_buffer.php
+++ src/breslin/clientside/network/check_for_byte_buffer.php
@@ -3,13 +3,28 @@
 $conn = pg_connect("host=localhost dbname=abcandyou user=postgres password=mibesfat")
        or die('Could not connect: ' . pg_last_error());
 
-$query = "select client_id from client where id = 1;";
+$query = "select id, position_x, position_z from shapes;";
 
 $result = pg_query($conn,$query);
-$row = pg_fetch_array($result, 0);
+$numrows = pg_numrows($result);
 
-$client_id = $row["client_id"];
 
 //send client id back to client browser
-echo $client_id;
+echo "<table border='1'>
+<tr>
+<th>id</th>
+<th>x</th>
+<th>z</th>
+</tr>";
+for($i = 0; $i < $numrows; $i++) 
+{
+$row = pg_fetch_array($result, $i);
+echo "<tr>";
+echo "<td>" . $row[0] . "</td>";
+echo "<td>" . $row[1] . "</td>";
+echo "<td>" . $row[2] . "</td>";
+echo "</tr>";
+}
+echo "</table>";
+
 ?>
