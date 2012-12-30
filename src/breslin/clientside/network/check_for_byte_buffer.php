aab2ef079c57c8cb77419f93da8407109f1181c6
jbreslin33 <jbreslin33@localhost>
2012-12-29 19:27:19 -0500

    cosmetic

--- src/breslin/clientside/network/check_for_byte_buffer.php
+++ src/breslin/clientside/network/check_for_byte_buffer.php
@@ -9,15 +9,7 @@ $result = pg_query($conn,$query);
 $row = pg_fetch_array($result, 0);
 
 $client_id = $row["client_id"];
-/*
-//now update the record
-$new_client_id = $client_id + 1;
-$query = "update client set client_id = ";
-$query .= $new_client_id;
-$query .= " where id = 1";
 
-$result = pg_query($conn,$query);
-*/
 //send client id back to client browser
 echo $client_id;
 ?>
