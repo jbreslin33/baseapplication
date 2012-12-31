eae4c19c0cc94faf9949bbcb55e5dd0f2e17881c
jbreslin3 <jbreslin3@localhost>
2012-12-30 23:33:07 -0500

    added functions to vector3d

--- src/breslin/math/vector_3d.php
+++ src/breslin/math/vector_3d.php
@@ -3,9 +3,58 @@ var Vector3D = new Class(
 	
 initialize: function()
 {
+	this.x = 0;
+	this.y = 0;
+	this.z = 0;
+},
 
+initialize: function(x1,y1,z1)
+{
+	this.x = x1;
+	this.y = y1;
+	this.z = z1;
+},
+
+length: function()
+{
+        //return sqrt(x*x + y*y + z*z);
+},
+
+isZero: function()
+{
+        if (this.x == 0.0 && this.y == 0.0 && this.z == 0.0)
+        {
+                return true;
+        }
+        else
+        {
+                return false;
+        }
+	
+},
+
+zero: function()
+{
+	this.x = 0
+	this.y = 0
+	this.z = 0
+},
+
+normalise: function()
+{
+	len = this.length();
+	if (len == 0)
+	{
+		return;
+	}
+	x /= len;
+	y /= len;
+	z /= len;
 }
 
 
-
 });
+/*
+
+
+*/
