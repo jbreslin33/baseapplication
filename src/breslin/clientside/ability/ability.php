76f34460464c49a98cf74eeb02998add6e47df9f
jbreslin33 <jbreslin33@localhost>
2013-01-02 16:20:02 -0500

    ability

--- src/breslin/clientside/ability/ability.php
+++ src/breslin/clientside/ability/ability.php
@@ -1,9 +1,21 @@
 var Ability = new Class(
 {
-        initialize: function (shape)
-        {
-                //shape
-                this.mShape = shape;      
-        }
+
+initialize: function (shape)
+{
+	//shape
+        this.mShape = shape;      
+},
+
+processTick(): function()
+{
+
+},
+
+interpolateTick: (renderTime)
+{
+
+}
+
 });
 
