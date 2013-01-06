37583f2757d5dbe9aed2a32326db9676081af0ba
jbreslin33 <jbreslin33@localhost>
2013-01-05 22:21:50 -0500

    still adding move states

--- src/breslin/clientside/ability/move/ability_move.php
+++ src/breslin/clientside/ability/move/ability_move.php
@@ -5,32 +5,7 @@ initialize: function (shapeDynamic)
 {
 	//shape
     	this.mShape = shapeDynamic;
-/*
-        //move processTick states
-        mProcessTickStateMachine = new AbilityMoveStateMachine(this);    //setup the state machine
-        mProcessTickStateMachine->setCurrentState      (Normal_ProcessTick_Move::Instance());
-        mProcessTickStateMachine->setPreviousState     (Normal_ProcessTick_Move::Instance());
-        mProcessTickStateMachine->setGlobalState       (Global_ProcessTick_Move::Instance());
-
-        //move interpolateTick states
-        mInterpolateTickStateMachine = new AbilityMoveStateMachine(this);    //setup the state machine
-        mInterpolateTickStateMachine->setCurrentState      (Normal_InterpolateTick_Move::Instance());
-        mInterpolateTickStateMachine->setPreviousState     (Normal_InterpolateTick_Move::Instance());
-        mInterpolateTickStateMachine->setGlobalState       (NULL);
-
-
-
-    //thresholds
-    mPosInterpLimitHigh = .066f; //how far away from server till we try to catch up
-    mPosInterpFactor    = 4.0f;
-        mMaximunVelocity    = .003083f; //do not let velocity go above this in any direction.
-
-        //deltas
-        mDeltaX        = 0.0f;
-        mDeltaY            = 0.0f;
-        mDeltaZ        = 0.0f;
-        mDeltaPosition = 0.0f;
-*/
+	
 	this.mGlobalProcessTickMove  = new GlobalProcessTickMove(this); 
 	this.mCatchupProcessTickMove = new CatchupProcessTickMove(this); 
 	this.mNormalProcessTickMove  = new NormalProcessTickMove(this); 
@@ -71,49 +46,36 @@ log: function(msg)
         }, 0);
 },
 
+/******************************************************
+*                               UPDATING
+********************************************************/
 processTick: function()
 {
-
+        this.mProcessTickStateMachine.update();
 },
 
 interpolateTick: function(renderTime)
 {
 //	this.log('intermove:' + renderTime);
-}
+        this.mInterpolateTickStateMachine.update();
+},
 
-});
-
-
-/******************************************************
-*                               UPDATING
-********************************************************/
-/*
-void AbilityMove::processTick()
-{
-        mProcessTickStateMachine->update();
-}
-void AbilityMove::interpolateTick(float renderTime)
-{
-        mInterpolateTickStateMachine->update();
-}
-*/
 /******************************************************
 *                               MOVE
 ********************************************************/
-/*
-void AbilityMove::calculateDeltaPosition()  //mov
+calculateDeltaPosition: function()
 {
-
-        mDeltaX = mShape->mServerCommandCurrent->mPosition->x - mShape->getPosition()->x;
-        mDeltaY = mShape->mServerCommandCurrent->mPosition->y - mShape->getPosition()->y;
-        mDeltaZ = mShape->mServerCommandCurrent->mPosition->z - mShape->getPosition()->z;
+        this.mDeltaX = this.mShape.mServerCommandCurrent.mPosition.x - this.mShape.getPosition().x;
+        this.mDeltaY = this.mShape.mServerCommandCurrent.mPosition.y - this.mShape.getPosition().y;
+        this.mDeltaZ = this.mShape.mServerCommandCurrent.mPosition.z - this.mShape.getPosition().z;
 
         //distance we are off from server
-        mDeltaPosition = sqrt(pow(mDeltaX, 2) + pow(mDeltaY, 2) +  pow(mDeltaZ, 2));
-}
+        //mDeltaPosition = sqrt(pow(mDeltaX, 2) + pow(mDeltaY, 2) +  pow(mDeltaZ, 2));
+},
 
-float AbilityMove::calcuateSpeed(Vector3D* velocity, int frameTime)
+calculateSpeed: function(velocity,frameTime)
 {
+/*
         float speed = sqrt(
         pow(velocity->x, 2) +
         pow(velocity->y, 2) +
@@ -121,7 +83,7 @@ float AbilityMove::calcuateSpeed(Vector3D* velocity, int frameTime)
         frameTime;
 
         return speed;
+*/
 }
 
-*/
-
+});
