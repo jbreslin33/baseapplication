9c96abd77d19d9535facbdf428319fe05d0e823b
jbreslin33 <jbreslin33@localhost>
2013-01-06 18:43:52 -0500

    crap value in velocity

--- src/breslin/clientside/ability/move/normal_interpolate_tick_move.php
+++ src/breslin/clientside/ability/move/normal_interpolate_tick_move.php
@@ -22,7 +22,39 @@ enter: function()
 
 execute: function()
 {
+        //to be used to setPosition
+        transVector = new Vector3D();
 
+        //copy values from mVelocity so we don't make changes to original
+        transVector.copyValuesFrom(this.mAbilityMove.mShape.mCommandToRunOnShape.mVelocity);
+
+	crap value here	       
+	this.log('x:' + this.mAbilityMove.mShape.mCommandToRunOnShape.mVelocity.x);
+        this.log('z:' + this.mAbilityMove.mShape.mCommandToRunOnShape.mVelocity.z);
+        
+        
+	
+        //get the mulitplier
+        multipliedRenderTime = this.mAbilityMove.mShape.mApplicationBreslin.getRenderTime() * 1000;
+
+        //multiply our vector using render values
+        transVector.multiply(multipliedRenderTime);
+
+        //add our velocity to current position
+        transVector.add(this.mAbilityMove.mShape.getPosition());
+
+	//set new position
+        this.mAbilityMove.mShape.setPosition(transVector);
+
+/*
+        if (abilityMove->mShape->mLocal == 1)
+        {
+                abilityMove->mShape->mApplicationBreslin->getCamera()->setPosition(Ogre::Vector3(transVector->x,transVector->y + 20,transVector->z + 20));
+                abilityMove->mShape->mApplicationBreslin->getCamera()->lookAt(Ogre::Vector3(transVector->x,transVector->y,transVector->z));
+
+        }
+
+*/
 },
 
 exit: function()
