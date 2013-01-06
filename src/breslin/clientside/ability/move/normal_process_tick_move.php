3b2fb79c8cb455940677d0951d505c2edd289e31
jbreslin33 <jbreslin33@localhost>
2013-01-06 18:01:08 -0500

    normal_processTick done

--- src/breslin/clientside/ability/move/normal_process_tick_move.php
+++ src/breslin/clientside/ability/move/normal_process_tick_move.php
@@ -14,30 +14,6 @@ enter: function()
 
 execute: function()
 {
-/*
-        //abilityMove->mShape->appendToTitle("M:Normal");
-
-        // if distance exceeds threshold && server velocity is zero
-        if(abilityMove->mDeltaPosition > abilityMove->mPosInterpLimitHigh && !abilityMove->mShape->mServerCommandCurrent->mVelocity->isZero())
-        {
-                abilityMove->mProcessTickStateMachine->changeState(Catchup_ProcessTick_Move::Instance());
-        }
-
-        Vector3D* serverVelocity = new Vector3D();
-        serverVelocity->copyValuesFrom(abilityMove->mShape->mServerCommandCurrent->mVelocity);
-        serverVelocity->normalise();
-
-        if(abilityMove->mShape->mCommandToRunOnShape->mFrameTime != 0)
-        {
-                abilityMove->mShape->mSpeed = abilityMove->calcuateSpeed(
-                abilityMove->mShape->mServerCommandCurrent->mVelocity,
-                abilityMove->mShape->mCommandToRunOnShape->mFrameTime);
-        }
-
-        serverVelocity->multiply(abilityMove->mShape->mSpeed);
-        //abilityMove->regulate(serverVelocity);
-        abilityMove->mShape->mCommandToRunOnShape->mVelocity->copyValuesFrom(serverVelocity);
-*/
         // if distance exceeds threshold && server velocity is zero
         if(this.mAbilityMove.mDeltaPosition > this.mAbilityMove.mPosInterpLimitHigh && !this.mAbilityMove.mShape.mServerCommandCurrent.mVelocity.isZero())
         {
@@ -56,10 +32,7 @@ execute: function()
 
         serverVelocity.multiply(this.mAbilityMove.mShape.mSpeed);
         //abilityMove->regulate(serverVelocity);
-/*
-        abilityMove->mShape->mCommandToRunOnShape->mVelocity->copyValuesFrom(serverVelocity);
-*/
-
+        this.mAbilityMove.mShape.mCommandToRunOnShape.mVelocity.copyValuesFrom(serverVelocity);
 },
 
 exit: function()
