300d1a527ecd6a9a8cc70231fbcd2da11a6a3186
jbreslin33 <jbreslin33@localhost>
2013-01-13 15:54:57 -0500

    cleanup

--- src/udp_server.js
+++ src/udp_server.js
@@ -40,10 +40,11 @@ server.on("message", function (msg, rinfo)
 	console.log('type:' + type);
 });
 
-server.on("listening", function () {
-  var address = server.address();
-  console.log("server listening " +
-      address.address + ":" + address.port);
+server.on("listening", function ()
+{
+	var address = server.address();
+  	console.log("server listening " +
+      	address.address + ":" + address.port);
 });
 
 server.bind(41234);
