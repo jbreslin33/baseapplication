c1fcdab37d5e9b8e14ebfde7d9e5604b3ce06f24
jbreslin33 <jbreslin33@localhost>
2013-01-13 15:52:12 -0500

    cleanup

--- src/udp_server.js
+++ src/udp_server.js
@@ -36,9 +36,6 @@ io.sockets.on('connection', function (socket)
 
 server.on("message", function (msg, rinfo)
 {
-//console.log("server got: " + msg + " from " +
- //   rinfo.address + ":" + rinfo.port);
-
         var type = msg.readInt8(0);
 	console.log('type:' + type);
 });
@@ -50,79 +47,3 @@ server.on("listening", function () {
 });
 
 server.bind(41234);
-
-
-/*
-	app.on('message', function (message, remote)
-	{
-        	//var type = message.readInt8(0);
-
-        	//console.log(remote.address + ':' + remote.port +' - ' + type);
-		console.log('against the wind');
-	});
-*/
-
-//});
-/*
-app.on('message', function (message, remote)
-{
-        //var type = message.readInt8(0);
-
-        //console.log(remote.address + ':' + remote.port +' - ' + type);
-	console.log('against the wind');
-});
-*/
-
-
-
-
-//var io = require('socket.io').listen(app);
-
-
-/*
-server.listen(8000);
-
-// routing
-app.get('/', function (req, res)
-{
-	res.sendfile(__dirname + '/main.html');
-});
-
-server.on('listening', function ()
-{
-	var address = server.address();
-    	console.log('UDP Server listening on ' + address.address + ":" + address.port);
-});
-
-server.on('connect', function ()
-{
-	console.log('successfully established a working connection \o/');
-});
-
-server.on('message', function (message, remote)
-{
-	var type = message.readInt8(0);
-	
-    	console.log(remote.address + ':' + remote.port +' - ' + type);
-});
-*/
-
-/*
-var PORT = 33333;
-var HOST = '192.168.1.101';
-
-var dgram = require('dgram');
-var server = dgram.createSocket('udp4');
-*/
-//server.bind(PORT, HOST);
-
-//send connect
-var buf = new Buffer(1);
-buf.writeInt8(-101,0);
-/*
-server.send(buf, 0, buf.length, 30004, HOST, function(err, bytes)
-{
-	console.log('sent connect');
-});
-*/
-
