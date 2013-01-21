5ae55f00460e4025c9c6b194649e2a7c8ce213e0
jbreslin33 <jbreslin33@localhost>
2013-01-21 18:46:27 -0500

    flags is which one?

--- src/udp_server.js
+++ src/udp_server.js
@@ -29,9 +29,26 @@ io.sockets.on('connection', function (socket)
 	});
 });
 
+
+
+
 server.on("message", function (msg, rinfo)
 {
-        var type   = msg.readInt8(0);
+	//constants
+	mCommandOriginX      = 4;
+	mCommandOriginY      = 8;
+	mCommandOriginZ      = 16;
+	mCommandRotationX    = 32;
+	mCommandRotationZ    = 64;
+
+
+	var length = msg.length;
+	var count = 0;
+
+        var type   = msg.readInt8(count);
+	var dataString = type;  
+
+	count++;
 	
 	//add shape
 	if (type == -103)
@@ -49,78 +66,79 @@ server.on("message", function (msg, rinfo)
 		console.log('t:' + type + 'c:' + client + 'i:' + index + 'x:' + xpos + 'y:' + ypos + 'z:' + zpos + 'x:' + xrot + 'z:' + zrot + 'm:' + mesh + 'a:' + anim);
 	
 		//let's just pass off data msg to browsers
-		var datastring = type + "," + client + "," + index + "," + xpos + "," + ypos + "," + zpos + "," + xrot + "," + zrot + "," + mesh + "," + anim; 
+		datastring = dataString + "," + client + "," + index + "," + xpos + "," + ypos + "," + zpos + "," + xrot + "," + zrot + "," + mesh + "," + anim; 
 		io.sockets.emit('news', datastring)
 	}
 
 	if (type == 1)
 	{
+		while(count < length)
+		{
+        		moveXChanged = true;
+        		moveYChanged = true;
+        		moveZChanged = true;
 		
-		//constants
-		mCommandOriginX      = 4;
-		mCommandOriginY      = 8;
-		mCommandOriginZ      = 16;
-		mCommandRotationX    = 32;
-		mCommandRotationZ    = 64;
+			dataString = type;  
 
-
-        	var s = msg.readUInt16LE(1);
-		var dataString = s;  
+        		dataString = dataString + "," + msg.readUInt16LE(count);
+			count = count + 2;
         	
-		var f = msg.readInt8(3);
-		dataString = dataString + f;
+			dataString = dataString + "," + msg.readInt8(count);
+			count++;
 		
-		var id = msg.readInt8(4);
-		dataString = dataString + id;
+			dataString = dataString + "," +  msg.readInt8(count);
+			count++;
 		
-		var flags = msg.readInt8(5);
-		dataString = dataString + flags;
+			dataString = dataString + "," +  msg.readInt8(count);
+			count++;
 
-        	// Origin
-        	if(flags & mCommandOriginX)
-        	{
-                	//mServerCommandLast->mPosition->x = mServerCommandCurrent->mPosition->x;
-                	//mServerCommandCurrent->mPosition->x = mes->ReadFloat();
-        		var xpos   = msg.readFloatLE(6);
-			dataString
-        	}
-        	else
-        	{
-                	moveXChanged = false;
-        	}
+        		// Origin
+        		if(flags & mCommandOriginX)
+        		{
+        			dataString = dataString + "," + msg.readFloatLE(count);
+				count = count + 4;
+        		}
+        		else
+        		{
+                		moveXChanged = false;
+        		}
 
-        	if(flags & mCommandOriginY)
-        	{
-                	mServerCommandLast->mPosition->y = mServerCommandCurrent->mPosition->y;
-                	mServerCommandCurrent->mPosition->y = mes->ReadFloat();
-        	}
-        	else
-        	{
-                	moveYChanged = false;
-        	}
+        		if(flags & mCommandOriginY)
+        		{
+        			dataString = dataString + "," + msg.readFloatLE(count);
+				count = count + 4;
+			}
+        		else
+        		{
+                		moveYChanged = false;
+        		}
 
-        	if(flags & mCommandOriginZ)
-        	{
-                	mServerCommandLast->mPosition->z = mServerCommandCurrent->mPosition->z;
-                	mServerCommandCurrent->mPosition->z = mes->ReadFloat();
-        	}
-        	else
-        	{
-                	moveZChanged = false;
-        	}
+        		if(flags & mCommandOriginZ)
+        		{
+        			dataString = dataString + "," + msg.readFloatLE(count);
+				count = count + 4;
+        		}
+        		else
+        		{
+                		moveZChanged = false;
+        		}
 
-        	//rotation
-        	if(flags & mCommandRotationX)
-        	{
-                	mServerCommandLast->mRotation->x = mServerCommandCurrent->mRotation->x;
-                	mServerCommandCurrent->mRotation->x = mes->ReadFloat();
-        	}
+        		//rotation
+        		if(flags & mCommandRotationX)
+        		{
+        			dataString = dataString + "," + msg.readFloatLE(count);
+				count = count + 4;
+        		}
 
-        	if(flags & mCommandRotationZ)
-        	{
-                	mServerCommandLast->mRotation->z = mServerCommandCurrent->mRotation->z;
-                	mServerCommandCurrent->mRotation->z = mes->ReadFloat();
-        	}
+        		if(flags & mCommandRotationZ)
+        		{
+        			dataString = dataString + "," + msg.readFloatLE(count);
+				count = count + 4;
+        		}
+
+		} //end if
+		console.log("dataString:" + dataString);	
+		io.sockets.emit('news', datastring)
 
 	}
 
