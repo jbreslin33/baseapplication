27fda433c5c977d2b39b49e44c8bea5d404124a5
jbreslin33 <jbreslin33@localhost>
2013-01-29 10:34:09 -0500

    we can move no problem over internet in browser

--- src/breslin/clientside/ability/move/normal_process_tick_move.php
+++ src/breslin/clientside/ability/move/normal_process_tick_move.php
@@ -22,37 +22,27 @@ enter: function()
 
 execute: function()
 {
-        this.log(this.mAbilityMove.mShape.mCommandToRunOnShape.mFrameTime);
-	
-//	this.log('ex normal process tick');
         // if distance exceeds threshold && server velocity is zero
-/*
         if(this.mAbilityMove.mDeltaPosition > this.mAbilityMove.mPosInterpLimitHigh && !this.mAbilityMove.mShape.mServerCommandCurrent.mVelocity.isZero())
         {
-                this.mAbilityMove.mProcessTickStateMachine.changeState(this.mAbilityMove.mCatchupProcessTickMove);
+       //         this.mAbilityMove.mProcessTickStateMachine.changeState(this.mAbilityMove.mCatchupProcessTickMove);
         }
-*/
-//	this.log('x:' + this.mAbilityMove.mShape.mServerCommandCurrent.mVelocity.x); 
-//	this.log('z:' + this.mAbilityMove.mShape.mServerCommandCurrent.mVelocity.z); 
-        serverVelocity = new Vector3D();
+        
+	serverVelocity = new Vector3D();
 
         serverVelocity.copyValuesFrom(this.mAbilityMove.mShape.mServerCommandCurrent.mVelocity);
         serverVelocity.normalise();
 
-//        this.log('mFrameTime:' + this.mAbilityMove.mShape.mCommandToRunOnShape.mFrameTime)
         if(this.mAbilityMove.mShape.mCommandToRunOnShape.mFrameTime != 0)
         {
                 v = this.mAbilityMove.mShape.mServerCommandCurrent.mVelocity;
                 f = this.mAbilityMove.mShape.mCommandToRunOnShape.mFrameTime;
 		
                 this.mAbilityMove.mShape.mSpeed = this.mAbilityMove.calculateSpeed(v,f);
-//		this.log('speed:' + this.mAbilityMove.mShape.mSpeed);
         }
         serverVelocity.multiply(this.mAbilityMove.mShape.mSpeed);
-//	this.log('x:' + serverVelocity.x); 
-//	this.log('z:' + serverVelocity.z); 
-        //abilityMove->regulate(serverVelocity);
-        this.mAbilityMove.mShape.mCommandToRunOnShape.mVelocity.copyValuesFrom(serverVelocity);
+        
+	this.mAbilityMove.mShape.mCommandToRunOnShape.mVelocity.copyValuesFrom(serverVelocity);
 },
 
 exit: function()
