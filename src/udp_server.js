93acf9c6114028ceae265c12f96987615cc9a55b
jbreslin33 <jbreslin33@localhost>
2013-03-26 19:37:00 -0400

    cosmetic

--- src/udp_server.js
+++ src/udp_server.js
@@ -103,25 +103,6 @@ io.sockets.on('connection', function (socket)
 		for (u = 0; u < usernameArraySize; u++)
 		{
 			var index = parseInt(u + 3);
-/*
-			if (usernameArray[u] == '0')
-			{
-				usernameArray[u] = '48';
-			}
-			if (usernameArray[u] == '1')
-			{
-				usernameArray[u] = '49'; 
-			}
-			if (usernameArray[u] == '2')
-			{
-				usernameArray[u] = '50'; 
-			}
-			if (usernameArray[u] == '3')
-			{
-				usernameArray[u] = '51'; 
-			}
-*/
-
 			var charCode = usernameArray[u].charCodeAt(0);
 			buf.writeInt8(charCode,index);
 			console.log(charCode);
@@ -141,51 +122,6 @@ io.sockets.on('connection', function (socket)
                 {
                 });
         });
-
-
-/*
- 	socket.on('send_login', function(message,remote)
-        {
-                mMessage = message;
-                var messageArray = message.split(" ");
-
-                var username = messageArray[0];                    
-		var usernameArray = username.split("");
-
-                var password = messageArray[1];                    
-		var passwordArray = password.split("");
-
-		var sizeOfUsername = parseInt(usernameArray.length);	
-		var sizeOfPassword = parseInt(passwordArray.length);	
-
-		var sizeOfBuffer = 4 + sizeOfUsername + sizeOfPassword; 
-		console.log('sizeOfBuffer:' + sizeOfBuffer);
-
-		type = -125;
-
-                //send to c++ server
-                //var buf = new Buffer(parseInt(sizeOfBuffer));
-                var buf = new Buffer(2);
-                buf.writeInt8(type,0);
-                buf.writeInt8(socket.mClientID,1);
-
-                buf.writeInt8(sizeOfUsername,2);
-		for (i = 0; i < sizeOfUsername; i++)
-		{
-                	buf.writeInt8(usernameArray[i],i+3);
-		}
-
-                buf.writeInt8(sizeOfPassword,2);
-		for (b = 0; b < sizeOfPassword; b++)
-		{
-                	buf.writeInt8(passwordArray[b],b+4+sizeOfUsername);
-		}
-
-                server.send(buf, 0, buf.length, mServerPort, mServerIP, function(err, bytes)
-                {
-                });
-        });
-*/
 });
 
 server.on("message", function (msg, rinfo)
