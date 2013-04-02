0fe39257bfa833a3bd4930ae13684bd5c7533b3a
jbreslin33 <jbreslin33@localhost>
2013-04-02 19:57:03 -0400

    cosmetic

--- src/breslin/serverside/client/client.cpp
+++ src/breslin/serverside/client/client.cpp
@@ -158,11 +158,9 @@ void Client::sendSchools()
         	mServer->mMessage.WriteByte(mServer->mMessageAddSchool); // add type
 		if (mClientID > 0)
 		{
-			LogString("sending mAddSchool to browser");
         		mServer->mMessage.WriteByte(mClientID); // add mClientID for browsers 
 		}
 		int length = mServer->mSchoolVector.at(i).length();  // get length of string containing school 
-		LogString("lengthinsendsch:%d",length);
 		mServer->mMessage.WriteByte(length); //send length 
 
 		//loop thru length and write it 
