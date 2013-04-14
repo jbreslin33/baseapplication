8e8f7dcc706d0e0f81e61e4f00d114edcaa56648
jbreslin33 <jbreslin33@localhost>
2013-04-13 22:22:42 -0400

    changing over more client

--- src/breslin/serverside/client/client.cpp
+++ src/breslin/serverside/client/client.cpp
@@ -123,7 +123,7 @@ void Client::joinGame(Game* game)
 	}
 
 	//create the shape for this client -- the avatar
-	mShape = new Shape(mGame->getOpenIndex(),mServer->mGame,this,mServer->mGame->getOpenPoint(),new Vector3D(),new Vector3D(),mServer->mGame->mRoot,true,true,.66f * 30.5,1,false); 
+	mShape = new Shape(mGame->getOpenIndex(),mGame,this,mGame->getOpenPoint(),new Vector3D(),new Vector3D(),mGame->mRoot,true,true,.66f * 30.5,1,false); 
 }
 
 void Client::leaveGame()
