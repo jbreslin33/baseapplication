7b34fe345f6e1074d7e78ff8eebbb99ce4a17988
jbreslin33 <jbreslin33@localhost>
2013-04-18 10:16:40 -0400

    addig gameMath where i hope to send math questions...

---
+++ src/breslin/serverside/game/gameMath.h
@@ -0,0 +1,73 @@
+#ifndef GAME_H
+#define GAME_H
+
+#include <string.h>
+
+#include <vector>
+
+//postgres
+#include <postgresql/libpq-fe.h>
+
+using namespace std;
+
+#define COMMAND_HISTORY_SIZE		64
+
+//class Network;
+class Message;
+class Server;
+class Client;
+class Shape;
+class Vector3D;
+class Bounds;
+
+class Game
+{
+public:
+
+//statics
+static const char mMessageKey        = 1;
+static const char mMessageFrameTime  = 2;
+static const char mMessageServerExit = 3;
+
+	int mID;
+
+	Server	*mServer;  //go between for game(contains game logic) and Network(handles sending message across internets)
+
+	//bounds
+	Bounds* mBounds;	
+
+	//shapes
+	std::vector<Shape*> mShapeVector;	//every tangible item in game world..
+
+	//db
+     	PGconn* mDBConnection;
+
+public:
+	Game(Server* server, int id);
+	~Game();
+
+	//index
+	unsigned int getOpenIndex ();
+	Vector3D* getOpenPoint    ();
+
+	//end game
+	virtual void checkForEndOfGame() { }
+
+	// Network
+	void	sendExitNotification();
+	void	readDeltaMoveCommand(Message *mes, Client *client);
+	
+	//time
+	void	processUpdate();
+	
+	//collision detection
+	virtual void checkCollisions();
+	virtual void collision(Shape* shape1, Shape* shape2);
+	virtual void checkBounds(Shape* shape);
+		
+	//scope
+ 	bool    checkScope(Client* client, Shape* shape);
+
+};
+
+#endif
