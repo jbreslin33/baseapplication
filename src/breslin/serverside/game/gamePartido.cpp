fa439e17bde2dcfc2c5a8c6d76dd9140d25e80d2
jbreslin33 <jbreslin33@localhost>
2013-04-25 21:08:46 -0400

    recognizing battle shapes. and only creating one battle with them. next: what to do with them?

--- src/breslin/serverside/game/gamePartido.cpp
+++ src/breslin/serverside/game/gamePartido.cpp
@@ -41,15 +41,29 @@ void GamePartido::collision(Shape* shape1, Shape* shape2)
 	//do regular collision of backing off shapes
 	Game::collision(shape1,shape2);	
 
+	bool battleCollision = true;
+
 	//now let's check if these guys are already in a battle
+	for (unsigned int i = 0; i < mBattleVector.size(); i++)
+	{
+		for (unsigned int s = 0; s < mBattleVector.at(i)->mShapeVector.size(); s++)
+		{
+			if (shape1 == mBattleVector.at(i)->mShapeVector.at(s))
+			{
+				battleCollision = false;	
+			} 
+		}
+	}
 
+	if (battleCollision)
+	{
 
+		std::vector<Shape*> shapeVector;
+		shapeVector.push_back(shape1);
+		shapeVector.push_back(shape2);
 
-	std::vector<Shape*> shapeVector;
-	shapeVector.push_back(shape1);
-	shapeVector.push_back(shape2);
-
-	//create a battle
-	Battle* battle = new Battle(this,shapeVector);
-	mBattleVector.push_back(battle);
+		//create a battle
+		Battle* battle = new Battle(this,shapeVector);
+		mBattleVector.push_back(battle);
+	}
 }
