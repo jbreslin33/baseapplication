459951548c2feb8f098f90ae13c322275416915c
jbreslin33 <jbreslin33@localhost>
2013-05-02 13:37:48 -0400

    adding php new state game_partido play

---
+++ src/breslin/clientside/game/states/game_partido_play.php
@@ -0,0 +1,37 @@
+var GamePlay = new Class(
+{
+
+initialize: function(game)
+{
+	this.mGame = game; 
+},
+
+log: function(msg)
+{
+        setTimeout(function()
+        {
+                throw new Error(msg);
+        }, 0);
+},
+
+enter: function()
+{
+
+},
+
+execute: function()
+{
+	//user input
+        this.mGame.processInput();
+
+        //network outgoing
+        this.mGame.sendByteBuffer();
+},	
+
+exit: function()
+{
+
+}
+
+});
+
