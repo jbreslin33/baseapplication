24b1f59cb809236c570eb0b9484b96c0f77c3379
jbreslin33 <jbreslin33@localhost>
2013-05-12 23:01:09 -0400

    took out windows from client

--- src/breslin/serverside/client/client.h
+++ src/breslin/serverside/client/client.h
@@ -5,29 +5,17 @@
 
 #include "../message/message.h"
 
-#ifdef WIN32
-	#pragma comment (lib,"ws2_32.lib")
-#pragma message ("Auto linking WinSock2 library")
+#include <string.h>
+#include <netinet/in.h>
 
-	#include <winsock2.h>
-#else
-	#include <string.h>
-	#include <netinet/in.h>
+// Define SOCKET data type for UNIX 
+typedef int SOCKET;
+
+#ifndef TRUE
+#define TRUE 1
 #endif
-
-// Define SOCKET data type for UNIX (defined in WinSock for Win32)
-// And socklen_t for Win32
-#ifdef WIN32
-	typedef int socklen_t;
-#else
-	typedef int SOCKET;
-
-	#ifndef TRUE
-	#define TRUE 1
-	#endif
-	#ifndef FALSE
-	#define FALSE 0
-	#endif
+#ifndef FALSE
+#define FALSE 0
 #endif
 
 // Connection states
@@ -36,11 +24,7 @@
 #define DREAMSOCK_DISCONNECTING			2
 #define DREAMSOCK_DISCONNECTED			4
 
-#ifdef WIN32
-	#define DREAMSOCK_INVALID_SOCKET	INVALID_SOCKET
-#else
-	#define DREAMSOCK_INVALID_SOCKET	-1
-#endif
+#define DREAMSOCK_INVALID_SOCKET	-1
 
 // System messages
 // Note (for all messages - system and user):
