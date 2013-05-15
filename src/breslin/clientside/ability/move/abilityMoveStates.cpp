27f7fe59c307b7e8370a4c837db14d6072286569
jbreslin33 <jbreslin33@localhost>
2013-05-15 12:00:20 -0400

    c++ is good moved camera. now php

--- src/breslin/clientside/ability/move/abilityMoveStates.cpp
+++ src/breslin/clientside/ability/move/abilityMoveStates.cpp
@@ -204,7 +204,7 @@ void Normal_InterpolateTick_Move::execute(AbilityMove* abilityMove)
 
 	if (abilityMove->mShape->mLocal == 1)
 	{
-		abilityMove->mShape->mApplication->getCamera()->setPosition(Ogre::Vector3(transVector->x,transVector->y + 320,transVector->z + 20));
+		abilityMove->mShape->mApplication->getCamera()->setPosition(Ogre::Vector3(transVector->x,transVector->y + 50,transVector->z + 20));
 
 		abilityMove->mShape->mApplication->getCamera()->lookAt(Ogre::Vector3(transVector->x,transVector->y,transVector->z));
 	
