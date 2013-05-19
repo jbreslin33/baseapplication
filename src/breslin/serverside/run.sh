6ef4245072714d27182913a20f0a92a9a3d35e1b
jbreslin33 <jbreslin33@localhost>
2013-05-19 15:26:55 -0400

    using port from passed in value

--- src/breslin/serverside/run.sh
+++ src/breslin/serverside/run.sh
@@ -3,7 +3,7 @@ pkill Server
 ./../vserver/dist/bin/Server $1 $2 &
 if test $3 -eq 1
 then
-node ../../udp.js 127.0.0.1 30001 10001 main_localhost_30001.html 40001 
+node ../../udp.js 127.0.0.1 $2 10001 main_localhost_30001.html 40001 
 fi
 
 
