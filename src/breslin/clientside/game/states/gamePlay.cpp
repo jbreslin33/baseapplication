c8da331729ec4256fb6c81ae03a812bb71ff32bd
jbreslin33 <jbreslin33@localhost>
2013-06-12 23:06:28 -0400

    needs work with input during move..

--- src/breslin/clientside/game/states/gamePlay.cpp
+++ src/breslin/clientside/game/states/gamePlay.cpp
@@ -33,7 +33,56 @@ void GamePlay::enter()
 void GamePlay::execute()
 {
 	//user input
-	mGame->processInput();	
+//	mGame->processInput();	
+
+ 	mGame->mKeyCurrent = 0;
+  	if (mGame->mApplication->mKeyArray[119]) //forward
+        {
+        //	mGame->mApplication->mKeyArray[119] = false;
+                mGame->mKeyCurrent |= mGame->mKeyUp;
+        }
+  	if (mGame->mApplication->mKeyArray[119] == false) //forward
+        {
+		LogString("falselllll");
+        	//mGame->mApplication->mKeyArray[119] = false;
+               // mGame->mKeyCurrent |= mGame->mKeyUp;
+        }
+
+
+/*
+ mKeyCurrent = 0;
+
+        if (mApplication->getKeyboard()->isKeyDown(OIS::KC_I)) // Forward
+        {
+                mKeyCurrent |= mKeyUp;
+        }
+
+        if (mApplication->getKeyboard()->isKeyDown(OIS::KC_K)) // Backward
+        {
+                mKeyCurrent |= mKeyDown;
+        }
+
+        if (mApplication->getKeyboard()->isKeyDown(OIS::KC_J)) // Left
+        {
+                mKeyCurrent |= mKeyLeft;
+        }
+
+        if (mApplication->getKeyboard()->isKeyDown(OIS::KC_L)) // Right
+        {
+                mKeyCurrent |= mKeyRight;
+        }
+
+        if (mApplication->getKeyboard()->isKeyDown(OIS::KC_Z)) // Rotate -Yaw(counter-clockwise)
+        {
+                mKeyCurrent |= mKeyCounterClockwise;
+        }
+
+        if (mApplication->getKeyboard()->isKeyDown(OIS::KC_X)) // Right + Yaw(clockwise)
+        {
+                mKeyCurrent |= mKeyClockwise;
+        }
+
+*/	
 	
 	//network outgoing
 	mGame->sendByteBuffer();
