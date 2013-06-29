81c95f08cdf9b28da0da2c0eab8a636dd6fa3492
jbresln33 <jbresln33@localhost>
2013-06-29 16:09:54 -0400

    u

--- src/breslin/serverside/ability/ai/states/aiPartidoStateMachine.h
+++ src/breslin/serverside/ability/ai/states/aiPartidoStateMachine.h
@@ -1,15 +1,15 @@
-#ifndef AISTATEMACHINE_H
-#define AISTATEMACHINE_H
-#include "aiState.h"
+#ifndef AIPARTIDOSTATEMACHINE_H
+#define AIPARTIDOSTATEMACHINE_H
+#include "aiPartidoState.h"
 
-class AI;
+class AIPartido;
 
-class AIStateMachine
+class AIPartidoStateMachine : AIStateMachine
 {
 private:
   //a pointer to the agent that owns this instance
-  AI*   m_pOwner;
-  AIState*    m_pCurrentState;
+  AIPartido*   mAIPartido;
+  AIPartidoState*    m_pCurrentState;
 
   //a record of the last state the agent was in
   AIState*   m_pPreviousState;
@@ -27,7 +27,6 @@ public:
 
   virtual ~AIStateMachine(){}
 
-  //use these methods to initialize the FSM
   void setCurrentState(AIState* s){m_pCurrentState = s;}
   void setGlobalState(AIState* s) {m_pGlobalState = s;}
   void setPreviousState(AIState* s){m_pPreviousState = s;}
@@ -35,46 +34,29 @@ public:
   //call this to update the FSM
   void  update()const
   {
-//    if a global state exists, call its execute method, else do nothing
     if(m_pGlobalState)   m_pGlobalState->execute(m_pOwner);
-    //same for the current state
-	    //Con::errorf("fieldPlayerStateMachine update");
     if (m_pCurrentState) m_pCurrentState->execute(m_pOwner);
   }
 
   //change to a new state
   void  changeState(AIState* pNewState)
   {
-   // assert(pNewState &&
-           //"<StateMachine::ChangeState>: trying to change to NULL state");
 
-    //keep a record of the previous state
     m_pPreviousState = m_pCurrentState;
 
-    //call the exit method of the existing state
 	if(m_pCurrentState)
        m_pCurrentState->exit(m_pOwner);
 
-    //change state to the new state
     m_pCurrentState = pNewState;
 
-    //call the entry method of the new state
 	if(m_pCurrentState)
        m_pCurrentState->enter(m_pOwner);
   }
 
-  //change state back to the previous state
   void  revertToPreviousState()
   {
     changeState(m_pPreviousState);
   }
-
-  //returns true if the current state's type is equal to the type of the
-  //class passed as a parameter.
- // bool  isInState(const State& st)const
- // {
- //   return typeid(*m_pCurrentState) == typeid(st);
- // }
 
   AIState*  currentState()  const{return m_pCurrentState;}
   AIState*  globalState()   const{return m_pGlobalState;}
