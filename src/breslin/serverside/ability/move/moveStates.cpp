96cc149e31162b03b74c77c6756ea2df57bd321f
jbreslin33 <jbreslin33@localhost>
2013-07-04 22:17:55 -0400

    cosmetic

--- src/breslin/serverside/ability/move/moveStates.cpp
+++ src/breslin/serverside/ability/move/moveStates.cpp
@@ -19,6 +19,9 @@
 //shape
 #include "../../shape/shape.h"
 
+/*****************************************
+	Normal_Move
+****************************************/
 Normal_Move* Normal_Move::Instance()
 {
   static Normal_Move instance;
@@ -29,24 +32,23 @@ void Normal_Move::enter(Move* move)
 }
 void Normal_Move::execute(Move* move)
 {
-	
 	//check for No_move and Decelerate and Accelerate states..
-    if (move->mShape->mKeyDirection.isZeroLength()) 
+    	if (move->mShape->mKeyDirection.isZeroLength()) 
 	{
 		if(move->mRunSpeed > 0.0) //Decelerate_Move
 		{
 			move->mMoveStateMachine->changeState(Decelerate_Move::Instance());
 			return;
 		}
-        else //No_Move
+        	else //No_Move
 		{
 			move->mMoveStateMachine->changeState(No_Move::Instance());
 			return;
 		}
-    }
+    	}
 	else 
 	{
-        if(move->mRunSpeed < move->mShape->mSpeedMax) //Accelerate_Move
+        	if(move->mRunSpeed < move->mShape->mSpeedMax) //Accelerate_Move
 		{
 			move->mMoveStateMachine->changeState(Accelerate_Move::Instance());
 			return;
@@ -63,6 +65,9 @@ void Normal_Move::exit(Move* move)
 {
 }
 
+/*****************************************
+	No_Move
+****************************************/
 No_Move* No_Move::Instance()
 {
 	static No_Move instance;
@@ -73,21 +78,21 @@ void No_Move::enter(Move* move)
 }
 void No_Move::execute(Move* move)
 {
-    if (move->mShape->mKeyDirection.isZeroLength()) 
+	if (move->mShape->mKeyDirection.isZeroLength()) 
 	{
 		if(move->mRunSpeed > 0.0) //Decelerate_Move
 		{
 			move->mMoveStateMachine->changeState(Decelerate_Move::Instance());
 			return;
 		}
-        else //No_Move
+        	else //No_Move
 		{
-           move->mRunSpeed = 0.0;
+           		move->mRunSpeed = 0.0;
 		}
-    }
+    	}
 	else 
 	{
-        if(move->mRunSpeed < move->mShape->mSpeedMax) //Accelerate_Move
+        	if(move->mRunSpeed < move->mShape->mSpeedMax) //Accelerate_Move
 		{
 			move->mMoveStateMachine->changeState(Accelerate_Move::Instance());
 			return;
@@ -103,6 +108,9 @@ void No_Move::exit(Move* move)
 {
 }
 
+/*****************************************
+	Accelerate_Move
+****************************************/
 Accelerate_Move* Accelerate_Move::Instance()
 {
 	static Accelerate_Move instance;
@@ -113,22 +121,22 @@ void Accelerate_Move::enter(Move* move)
 }
 void Accelerate_Move::execute(Move* move)
 {
-    if (move->mShape->mKeyDirection.isZeroLength()) 
+	if (move->mShape->mKeyDirection.isZeroLength()) 
 	{
 		if(move->mRunSpeed > 0.0) //Decelerate_Move
 		{
 			move->mMoveStateMachine->changeState(Decelerate_Move::Instance());
 			return;
 		}
-        else //No_Move
+        	else //No_Move
 		{
 			move->mMoveStateMachine->changeState(No_Move::Instance());
 			return;
 		}
-    }
+    	}
 	else 
 	{
-        if(move->mRunSpeed < move->mShape->mSpeedMax) //Accelerate_Move
+        	if(move->mRunSpeed < move->mShape->mSpeedMax) //Accelerate_Move
 		{
 			move->mRunSpeed += move->mRunAccel;
 		}
@@ -149,6 +157,9 @@ void Accelerate_Move::exit(Move* move)
 {
 }
 
+/*****************************************
+	Decelerate_Move
+****************************************/
 Decelerate_Move* Decelerate_Move::Instance()
 {
 	static Decelerate_Move instance;
@@ -159,21 +170,21 @@ void Decelerate_Move::enter(Move* move)
 }
 void Decelerate_Move::execute(Move* move)
 {
-    if (move->mShape->mKeyDirection.isZeroLength()) 
+    	if (move->mShape->mKeyDirection.isZeroLength()) 
 	{
 		if(move->mRunSpeed > 0.0) //Decelerate_Move
 		{
 			move->mRunSpeed -= move->mRunDecel;
 		}
-        else //No_Move
+        	else //No_Move
 		{
 			move->mMoveStateMachine->changeState(No_Move::Instance());
 			return;
 		}
-    }
+    	}
 	else 
 	{
-        if(move->mRunSpeed < move->mShape->mSpeedMax) //Accelerate_Move
+        	if(move->mRunSpeed < move->mShape->mSpeedMax) //Accelerate_Move
 		{
 			move->mMoveStateMachine->changeState(Accelerate_Move::Instance());
 			return;
