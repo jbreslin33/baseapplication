012ed126369f2bf47d20a2b7b8aa8f3cbbc4535c
jbreslin33 <jbreslin33@localhost>
2013-07-12 22:40:51 -0400

    took out includes

--- src/breslin/serverside/client/robust/clientRobust.cpp
+++ src/breslin/serverside/client/robust/clientRobust.cpp
@@ -4,48 +4,6 @@
 //log
 #include "../../tdreamsock/dreamSockLog.h"
 
-//message
-#include "../../message/message.h"
-
-//network
-#include "../../network/network.h"
-
-//server
-#include "../../server/server.h"
-
-//game
-#include "../../game/game.h"
-
-//shape
-#include "../../shape/shape.h"
-
-//rotation
-#include "../../rotation/rotation.h"
-
-//move
-#include "../../move/move.h"
-
-//math
-#include "../../../math/vector3D.h"
-
-//states
-#include "../states/clientStates.h"
-
-//mailman
-#include "../../mailman/mailMan.h"
-
-#ifdef WIN32
-//
-#else
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <arpa/inet.h>
-#endif
-
-//postgresql
-#include <stdio.h>
-#include <postgresql/libpq-fe.h>
-
 
 ClientRobust::ClientRobust(Server* server, struct sockaddr *address, int clientID, bool permanence) : Client(server,address,clientID,permanence)
 {
