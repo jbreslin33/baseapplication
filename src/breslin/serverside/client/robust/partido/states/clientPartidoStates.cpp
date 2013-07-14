a35081044237988cdf526599a207e96024b548ba
jbreslin33 <jbreslin33@localhost>
2013-07-14 12:37:16 -0400

    u

--- src/breslin/serverside/client/robust/partido/states/clientPartidoStates.cpp
+++ src/breslin/serverside/client/robust/partido/states/clientPartidoStates.cpp
@@ -1,3 +1,7 @@
+/***
+these states are always on...as their is always a gamePartido..and these clients are in it. it really should handle the mechanics of quiz... 
+
+*/
 //parent
 #include "clientPartidoStates.h"
 
@@ -10,6 +14,8 @@
 //ability
 #include "../clientPartido.h"
 
+//server
+#include "../../../../server/partido/serverPartido.h"
 
 /*****************************************
 *******       GLOBAL    ******************
@@ -34,3 +40,78 @@ bool GlobalClientPartido::onLetter(ClientPartido* clientPartido, Letter* letter)
         return false;
 }
 
+
+/*****************************************
+*******       CLIENT STATES    ******************       
+****************************************/
+
+/*****************************************
+        AJAX_NODE       
+****************************************/
+Waiting_For_Answer* Waiting_For_Answer::Instance()
+{
+  static Waiting_For_Answer instance;
+  return &instance; 
+} 
+void Waiting_For_Answer::enter(ClientPartido* clientPartido)
+{
+}       
+void Waiting_For_Answer::execute(ClientPartido* clientPartido)
+{
+}
+void Waiting_For_Answer::exit(ClientPartido* clientPartido)
+{
+
+}
+bool Waiting_For_Answer::onLetter(ClientPartido* clientPartido, Letter* letter)
+{
+        return false;   
+}
+
+/*****************************************
+        Sending_Question
+****************************************/
+Sending_Question* Sending_Question::Instance()
+{
+  static Sending_Question instance;
+  return &instance;
+}
+void Sending_Question::enter(ClientPartido* clientPartido)
+{
+        LogString("Sending_Question::enter");
+}
+void Sending_Question::execute(ClientPartido* clientPartido)
+{
+}
+void Sending_Question::exit(ClientPartido* clientPartido)
+{
+}
+bool Sending_Question::onLetter(ClientPartido* clientPartido, Letter* letter)
+{
+        return false; 
+}
+/*****************************************
+                GAME_MODE               
+****************************************/
+Game_Partido_Mode* Game_Partido_Mode::Instance()
+{
+  static Game_Partido_Mode instance;
+  return &instance;
+}
+void Game_Partido_Mode::enter(ClientPartido* clientPartido)
+{
+        LogString("Game_Partido_Mode");
+        clientPartido->mInGame = true;
+}
+void Game_Partido_Mode::execute(ClientPartido* clientPartido)
+{
+
+}
+void Game_Partido_Mode::exit(ClientPartido* clientPartido)
+{
+        clientPartido->mInGame = false; 
+}
+bool Game_Partido_Mode::onLetter(ClientPartido* clientPartido, Letter* letter)
+{
+        return false; 
+}
--- src/breslin/serverside/client/robust/partido/states/clientPartidoStates.h
+++ src/breslin/serverside/client/robust/partido/states/clientPartidoStates.h
@@ -22,4 +22,42 @@ public:
   bool onLetter(ClientPartido* clientPartido, Letter* letter);
 };
 
+/*******************************
+*       CLIENT PARTIDO STATES 
+******************************/
+class Waiting_For_Answer : public State<ClientPartido>
+{
+private:
+  Waiting_For_Answer(){}
+public:
+  static Waiting_For_Answer* Instance();
+  void enter  (ClientPartido* clientPartido);
+  void execute(ClientPartido* clientPartido);
+  void exit   (ClientPartido* clientPartido);
+  bool onLetter(ClientPartido* clientPartido, Letter* letter);
+};
+
+class Sending_Question : public State<ClientPartido>
+{
+private:
+  Sending_Question(){}
+public:
+  static Sending_Question* Instance();
+  void enter  (ClientPartido* clientPartido);
+  void execute(ClientPartido* clientPartido);
+  void exit   (ClientPartido* clientPartido);
+  bool onLetter(ClientPartido* clientPartido, Letter* letter);
+};
+
+class Game_Partido_Mode : public State<ClientPartido>
+{
+private:
+  Game_Partido_Mode(){}
+public:
+  static Game_Partido_Mode* Instance();
+  void enter  (ClientPartido* clientPartido);
+  void execute(ClientPartido* clientPartido);
+  void exit   (ClientPartido* clientPartido);
+  bool onLetter(ClientPartido* clientPartido, Letter* letter);
+};
 #endif
