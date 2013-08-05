f21227ee14f86896180b05a6771e91833ad1df1a
jbreslin33 <jbreslin33@localhost>
2013-08-05 14:46:53 -0400

    ghost not visible

--- src/breslin/clientside/shape/shape.cpp
+++ src/breslin/clientside/shape/shape.cpp
@@ -65,7 +65,7 @@ Shape::Shape(ApplicationBreslin* application, ByteBuffer* byteBuffer, bool isGho
 	{
 		//create a ghost for this shape
 		mGhost = new Shape(mApplication,byteBuffer,true);
-		mGhost->setVisible(true);
+		mGhost->setVisible(false);
 	}
 
 }
