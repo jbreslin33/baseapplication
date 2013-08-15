679210a5c5740881e9f87471cbe8cab050b066fa
jbreslin33 <jbreslin33@localhost>
2013-08-15 18:22:20 -0400

    add new state

---
+++ src/breslin/clientside/game/states/gamePlayPartidoShowCorrectAnswer.h
@@ -0,0 +1,36 @@
+#ifndef GAMEPLAYPARTIDOBATTLE_H
+#define GAMEPLAYPARTIDOBATTLE_H
+
+/***************************************
+*   		INCLUDES
+***************************************/
+
+#include "../../../statemachine/state.h"
+
+/***************************************
+*   		FORWARD DECLARATIONS
+***************************************/
+
+class GamePartido;
+
+class GamePlayPartidoBattle : public State
+{
+public:
+
+/***************************************
+*   		MEMBER VARIABLES
+***************************************/
+
+GamePartido* mGamePartido;
+bool mFirstTimeExecute;
+
+GamePlayPartidoBattle(GamePartido* gamePartido);
+~GamePlayPartidoBattle();
+
+void enter  ();
+void execute();
+void exit   ();
+
+};
+
+#endif
