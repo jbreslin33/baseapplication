f9aa657bb96458cc3e3d46b9a811f62f5a8dd663
jbreslin33 <jbreslin33@localhost>
2013-09-18 12:01:01 -0400

    c

--- src/breslin/clientside/ability/animation/abilityAnimationOgre.cpp
+++ src/breslin/clientside/ability/animation/abilityAnimationOgre.cpp
@@ -84,80 +84,79 @@ void AbilityAnimationOgre::runAnimations()
 void AbilityAnimationOgre::fadeAnimations(Real deltaTime)
 {
 	for (int i = 0; i < mNumberOfAnimations; i++)
-    {
+    	{
 		if (mFadingIn[i])
-        {
+        	{
 			// slowly fade this animation in until it has full weight
-            Real newWeight = mAnims[i]->getWeight() + deltaTime * mAnimationFadeSpeed;
-            mAnims[i]->setWeight(Math::Clamp<Real>(newWeight, 0, 1));
-            if (newWeight >= 1)
-            {
+            		Real newWeight = mAnims[i]->getWeight() + deltaTime * mAnimationFadeSpeed;
+            		mAnims[i]->setWeight(Math::Clamp<Real>(newWeight, 0, 1));
+            		if (newWeight >= 1)
+            		{
 				mFadingIn[i] = false;
-            }
-        }
-        else if (mFadingOut[i])
-        {
+            		}
+        	}
+        	else if (mFadingOut[i])
+        	{
 			// slowly fade this animation out until it has no weight, and then disable it
 			Real newWeight = mAnims[i]->getWeight() - deltaTime * mAnimationFadeSpeed;
-            mAnims[i]->setWeight(Math::Clamp<Real>(newWeight, 0, 1));
-            if (newWeight <= 0)
-            {
+            		mAnims[i]->setWeight(Math::Clamp<Real>(newWeight, 0, 1));
+            		if (newWeight <= 0)
+            		{
 				mAnims[i]->setEnabled(false);
-                mFadingOut[i] = false;
-            }
-        }
+                		mFadingOut[i] = false;
+            		}
+        	}
 	}
 }
 
 void AbilityAnimationOgre::setBaseAnimation(AnimID id, bool reset)
 {
 	if (mBaseAnimID >= 0 && mBaseAnimID < mNumberOfAnimations)
-    {
+    	{
 		// if we have an old animation, fade it out
-        mFadingIn[mBaseAnimID] = false;
-        mFadingOut[mBaseAnimID] = true;
-    }
+        	mFadingIn[mBaseAnimID] = false;
+        	mFadingOut[mBaseAnimID] = true;
+    	}
      
-    mBaseAnimID = id;
+    	mBaseAnimID = id;
 
-    if (id != ANIM_NONE)
-    {
+    	if (id != ANIM_NONE)
+    	{
 		// if we have a new animation, enable it and fade it in
-        mAnims[id]->setEnabled(true);
-        mAnims[id]->setWeight(0);
-        mFadingOut[id] = false;
-        mFadingIn[id] = true;
+        	mAnims[id]->setEnabled(true);
+        	mAnims[id]->setWeight(0);
+        	mFadingOut[id] = false;
+        	mFadingIn[id] = true;
 
 		if (reset)
-        {
+        	{
 			mAnims[id]->setTimePosition(0);
-        }
+        	}
 	}
 }
 
 void AbilityAnimationOgre::setTopAnimation(AnimID id, bool reset)
 {
-
 	if (mTopAnimID >= 0 && mTopAnimID < mNumberOfAnimations)
-    {
+    	{
 		// if we have an old animation, fade it out
-        mFadingIn[mTopAnimID] = false;
-        mFadingOut[mTopAnimID] = true;
-    }
+       		mFadingIn[mTopAnimID] = false;
+        	mFadingOut[mTopAnimID] = true;
+    	}
 
-    mTopAnimID = id;
+    	mTopAnimID = id;
 
-    if (id != ANIM_NONE)
-    {
+    	if (id != ANIM_NONE)
+    	{
 		// if we have a new animation, enable it and fade it in
-        mAnims[id]->setEnabled(true);
-        mAnims[id]->setWeight(0);
-        mFadingOut[id] = false;
-        mFadingIn[id] = true;
+        	mAnims[id]->setEnabled(true);
+        	mAnims[id]->setWeight(0);
+        	mFadingOut[id] = false;
+        	mFadingIn[id] = true;
 
-        if (reset)
-        {
+        	if (reset)
+        	{
 			mAnims[id]->setTimePosition(0);
-        }
+       		}
 	}
 }
