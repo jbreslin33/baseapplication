1b60c456a508b7ada00ab12ed08da830cd239df0
jbreslin33 <jbreslin33@localhost>
2013-10-31 08:21:33 -0400

    working on standings

--- src/breslin/serverside/game/game.cpp
+++ src/breslin/serverside/game/game.cpp
@@ -389,26 +389,18 @@ void Game::setStandings()
 		}
 	}
       
-	//add to standings vector in order of score 
-	//loop thru scores starting at high score...
-	for (unsigned int scoreInQuestion = highScore; scoreInQuestion > 0; scoreInQuestion--) 
+	//add to standings vector in order of lowest scores 
+	for (int score = 0; score <= highScore; score++) 
 	{
 		//loop thru all clients who have played	
 		for (unsigned int i = 0; i < mServer->mClientVector.size(); i++)
 		{
 			//is this clients score = to scoreInQuestion if so add to standings vector
-			if (mServer->mClientVector.at(i)->mScore == scoreInQuestion) 
+			if (mServer->mClientVector.at(i)->mScore == score) 
 			{	
-				//mClientStandingsVector.push_back(mServer->mClientVector.at(i));
-				mClientStandingsVector.insert(mClientStandingsVector.begin(),mServer->mClientVector.at(i));
+				mClientStandingsVector.push_back(mServer->mClientVector.at(i));
 			}
 		}
-	}
-	
-	for (unsigned int i = 0; i < mClientStandingsVector.size(); i++)
-	{
-		LogString("STANDINGS:");
-		LogString("ID:%d SCORE:%d",mClientStandingsVector.at(i)->id, mClientStandingsVector.at(i)->mScore);
 	}
 }
 
--- src/breslin/serverside/game/states/gameStates.cpp
+++ src/breslin/serverside/game/states/gameStates.cpp
@@ -131,41 +131,30 @@ void RESET_GAME::enter(Game* game)
 void RESET_GAME::execute(Game* game)
 {
 	//send out standings here....
-	
-	//this is where they want to move
-/*
-        for (unsigned int i = 0; i < game->mShapeVector.size(); i++)
-        {
-                game->mShapeVector.at(i)->update();
-                game->checkBounds(game->mShapeVector.at(i));
-        }
-*/
         if (game->mGameTime > game->mGameTimeEnd + game->mResetTime)
 	{
 		//go back in time for next one
 		game->mGameTime = game->mGameTime - game->mResetTime;
 
 		bool areWeDone = true;
-		LogString("size of standings vectori:%d",game->mClientStandingsVector.size());
         	for (unsigned int i = 0; i < game->mClientStandingsVector.size(); i++)
         	{
-			LogString("i:%d",i);
+			LogString("loop:%d",i);
 			if (game->mClientStandingsVector.at(i)->mStandingsSent == false)
 			{		
-				LogString("if i:%d",i);
+				LogString("if:%d",i);
 				game->mClientStandingsVector.at(i)->mStandingsSent = true;
 				areWeDone = false;
-				LogString("id:%d",game->mClientStandingsVector.at(i)->id);		
 				std::string s = "Place: ";
 				s.append(StringConverter::toString(i));
 				s.append(" ");
 				s.append(game->mClientStandingsVector.at(i)->first_name);
 				game->reportStandings(s);
-				//i = -2;
 			}
 		}
 		if (areWeDone)
 		{
+			LogString("we are done!!!!");
 			game->mStateMachine->changeState(NORMAL_GAME::Instance());	
 		}
 	}
